4f1552260be7b6f276af17afcc9c19dd9073b04a
Andrew Ormond <drewthedev9@gmail.com>
2019-09-11 23:35:15 -0400

    added responsive nav

---
+++ recover.js
@@ -0,0 +1,13 @@
+// open nav function for burger button.
+openNav =()=>{
+    document.getElementById('sidebar').style.width = '250px';
+    console.log("working");
+    
+  
+    };
+  
+  // close nav bar function.
+    closeNav=()=>{
+      document.getElementById('sidebar').style.width = '0';
+     
+      };
